7f4624702429f60d43ab6ce796fa79efa97b0a3b
Hanson34 <80374182+Hanson34@users.noreply.github.com>
2021-04-28 08:20:26 +0800

    v1.1 (comments included)

--- interface.c
+++ interface.c
@@ -4,76 +4,104 @@
 
 #include "interface.h"
 
-#define unitSize 10
-
 int runInterface(){
 	int option, w, h;
-	printf("\nWelcome to Game of Life\n1. New Game\n2. Load Game\n3. Quit\nEnter the option: ");
-    scanf("%d", &option);
-    getchar();
-    if(option==1){
-		printf("\nEnter the width and height(width*height) you want: ");
-		scanf("%d*%d",&w, &h);
-		getchar();
-		horizontalWidth=w;
-		verticalHeight=h;
-		int unit[verticalHeight][horizontalWidth];
-	    for(int unitY = 0; unitY < verticalHeight; unitY++)
-	        for(int unitX = 0; unitX < horizontalWidth; unitX++)
-	            unit[unitY][unitX] = 0;
-		screenWidth=w*unitSize;
-		screenHeight=h*unitSize;
-		sdlEvent(unit);
-	}
-	if(option==2){
-		int s;
-		int count=0;
-	    FILE *st = fopen("state.txt","r+");
-	    if(st==NULL)
-	        return -1;
-	    while(1){
-	    	s=fgetc(st);
-	    	if(s!='\n')
-	    		count++;
-	    	else
-	    		break;
+		printf("\nWelcome to Game of Life\n\nPress enter to evolve single step;\nPress space to evolve continuously;\nPress up to speed up;\nPress down to solwn down;\nPress backspace to clear the screen\n\n1. New Game\n2. Load Game\n3. Quit\nEnter the option: ");
+	    scanf("%d", &option);
+	    getchar();
+	    //create a new game
+	    if(option==1){
+	    	//set up the world range
+			printf("\nEnter the width and height(width*height) you want: ");
+			scanf("%d*%d",&w, &h);
+			getchar();
+			horizontalWidth=w;
+			verticalHeight=h;
+			//initialize the cell unit
+			int unit[verticalHeight][horizontalWidth];
+		    for(int unitY = 0; unitY < verticalHeight; unitY++)
+		        for(int unitX = 0; unitX < horizontalWidth; unitX++)
+		            unit[unitY][unitX] = 0;
+		    //set up the SDL window size
+			screenWidth=w*unitSize;
+			screenHeight=h*unitSize;
+			//continue working...
+			sdlEvent(unit);
 		}
-		fclose(st);
-		horizontalWidth=count;
-		verticalHeight=count;
-		screenWidth=horizontalWidth*unitSize;
-		screenHeight=verticalHeight*unitSize;
-		FILE *st1 = fopen("state.txt","r+");
-		int unit[verticalHeight][horizontalWidth];
-	    for(int y = 0; y < verticalHeight; y++){
-	        for(int x = 0; x < horizontalWidth; x++){
-				s=fgetc(st1);
-				if(s==48)
-					unit[y][x] = 0;
-				else
-					unit[y][x] = 1;
+		else if(option==2){
+			int s;
+			int count=0;
+			//read world size from file
+		    FILE *st = fopen("state.txt","r+");
+		    if(st==NULL)
+		        return -1;
+		    while(1){
+		    	s=fgetc(st);
+		    	if(s!='\n')
+		    		count++;
+		    	else
+		    		break;
 			}
-			fscanf(st1,"%c",&s);
+			fclose(st);
+			//set up the SDL window size
+			horizontalWidth=count;
+			verticalHeight=count;
+			screenWidth=horizontalWidth*unitSize;
+			screenHeight=verticalHeight*unitSize;
+			//extract from existing file to load the cell world
+			//0 stands for dead, 1 stands for alive
+			FILE *st1 = fopen("state.txt","r+");
+			int unit[verticalHeight][horizontalWidth];
+		    for(int y=0; y<verticalHeight; y++){
+		        for(int x=0; x<horizontalWidth; x++){
+					s=fgetc(st1);
+					if(s==48)
+						unit[y][x] = 0;
+					else
+						unit[y][x] = 1;
+				}
+				fscanf(st1,"%c",&s);
+			}
+			fclose(st1);
+			//continue working...
+			sdlEvent(unit);
 		}
-		fclose(st1);
-		sdlEvent(unit);
-	}
-	return 0;
+		return 0;
 }
 
-void sdlEvent(int cell[][horizontalWidth])
-{
+void sdlEvent(int cell[verticalHeight][horizontalWidth]){
 	SDL_Init(SDL_INIT_VIDEO);
+	//create and render the SDL window
     SDL_Window *w=sdlWindow("GoL");
     SDL_Renderer *r=rRenderer(w);
     SDL_Event e;
     bool clickL=false;
     bool clickR=false;
-     while(1){
+    bool pause=true;
+    int tick=1000;
+    int t=0;
+//    int option;
+//    printf("\n1. Evolve at will\n 2. Evolve centain steps\nEnter the choice: ");
+//    scanf("%d", &option);
+//	getchar();
+//	if(option==2){
+//		printf("\nEnter the steps: ");
+//		scanf("%d", &option);
+//		getchar();
+//		for(int i=0; i<option; i++){
+//			if(SDL_GetTicks()-t>=tick){
+//				evolve(cell);
+//				saveState(cell);
+//				t=SDL_GetTicks();
+//			}
+//		}
+//	}
+    while(1){
         while(SDL_PollEvent(&e)!=0){
             switch(e.type){
                 case SDL_QUIT:
                     return;
+                //left click to make cell alive, right click to make it dead
                 case SDL_MOUSEBUTTONDOWN:
 					case SDL_MOUSEBUTTONUP:
                 		switch(e.button.button){
@@ -85,11 +113,39 @@ void sdlEvent(int cell[][horizontalWidth])
 								break;
 		                }
                 case SDL_KEYDOWN:
-                    if(e.key.keysym.sym==SDLK_SPACE)
+                	//press enter to evolve
+                    if(e.key.keysym.sym==SDLK_RETURN)
 	                evolve(cell);
 	                saveState(cell);
+	                //press backspace to clear the screen
+	                if(e.key.keysym.sym==SDLK_BACKSPACE)
+		            for(int unitY=0; unitY<verticalHeight; unitY++)
+		        		for(int unitX=0; unitX<horizontalWidth; unitX++)
+		            		cell[unitY][unitX] = 0;
+		            //press space to start/pause the process
+		            if(e.key.keysym.sym==SDLK_SPACE){
+		            	if(pause==true){
+		            		pause=false;
+		            		break;
+						}
+						if(pause==false){
+							pause=true;
+							break;
+						}
+					}
+					//press up/down to change speed
+					if(e.key.keysym.sym==SDLK_UP)
+						tick=100;
+					if(e.key.keysym.sym==SDLK_DOWN)
+						tick=1000;
             }
         }
+		if(SDL_GetTicks()-t>=tick&&pause==false){
+			evolve(cell);
+			saveState(cell);
+			t=SDL_GetTicks();
+		}
+        //render to SDL window
     	int inputX, inputY;
         SDL_GetMouseState(&inputX, &inputY);
         if(clickL==true)
@@ -97,16 +153,19 @@ void sdlEvent(int cell[][horizontalWidth])
         else if(clickR==true)
             cell[inputY/unitSize][inputX/unitSize]=0;
         SDL_SetRenderDrawColor(r, 255, 255, 255, 255);
+        //clear the renderer
         SDL_RenderClear(r);
+        //redraw the plate
         drawPlate(r);
         drawCell(r, cell);
         SDL_RenderPresent(r);
     }
+    //quit when press the x or the window
     SDL_Quit();
 }
 
-SDL_Window *sdlWindow(char *title)
-{
+SDL_Window *sdlWindow(char *title){
+	//SDL_Window * SDL_CreateWindow(const char *title, int x, int y, int w, int h, Uint32 flags);
     SDL_Window *w=SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, 0);
 	if(w==NULL){
         printf("\nWindow not created\n%s\n", SDL_GetError());
@@ -115,9 +174,9 @@ SDL_Window *sdlWindow(char *title)
     return w;
 }
 
-SDL_Renderer *rRenderer(SDL_Window *w)
-{
-    SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED);
+SDL_Renderer *rRenderer(SDL_Window *w){
+	//SDL_Renderer * SDL_CreateRenderer(SDL_Window * window, int index, Uint32 flags);
+    SDL_Renderer *r=SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED);
 	if(r==NULL){
         printf("\nRenderer not created\n%s\n", SDL_GetError());
         exit(EXIT_FAILURE);
@@ -125,85 +184,43 @@ SDL_Renderer *rRenderer(SDL_Window *w)
     return r;
 }
 
-void drawPlate(SDL_Renderer *r)
-{
+void drawPlate(SDL_Renderer *r){
     for(int v=unitSize; v<screenWidth; v+=unitSize){
-        SDL_SetRenderDrawColor(r, 100, 100, 100, 100);
+    	//int SDL_SetRenderDrawColor(SDL_Renderer * renderer, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+        SDL_SetRenderDrawColor(r, 10, 10, 10, 255);
+        //int SDL_RenderDrawLine(SDL_Renderer * renderer, int x1, int y1, int x2, int y2);
         SDL_RenderDrawLine(r, v, 0, v, screenHeight);
     }
     for(int h=unitSize; h<screenHeight; h+=unitSize){
-        SDL_SetRenderDrawColor(r, 100, 100, 100, 100);
+        SDL_SetRenderDrawColor(r, 10, 10, 10, 255);
         SDL_RenderDrawLine(r, 0, h, screenWidth, h);
     }
 }
 
-void drawCell(SDL_Renderer *r, int cell[][horizontalWidth])
-{
+void drawCell(SDL_Renderer *r, int cell[verticalHeight][horizontalWidth]){
     SDL_Rect unit;
     unit.w=unitSize+1;
     unit.h=unitSize+1;
     for(int uY=0; uY<verticalHeight; uY++){
         for(int uX=0; uX<horizontalWidth; uX++){
             if(cell[uY][uX]==1){
-                unit.x = uX * unitSize;
-                unit.y = uY * unitSize;
-                SDL_SetRenderDrawColor(r, 250, 160, 0, 110);
+                unit.x=uX*unitSize;
+                unit.y=uY*unitSize;
+                SDL_SetRenderDrawColor(r, 10, 10, 10, 255);
+                //int SDL_RenderFillRect(SDL_Renderer * renderer, const SDL_Rect * rect);
                 SDL_RenderFillRect(r, &unit);
             }
         }
     }
 }
 
-//void evolve(int cell[][horizontalWidth])
-//{
-//	int buf[verticalHeight][horizontalWidth];
-//	int neighbour;
-//    for(int j=0; j<horizontalWidth; j++){
-//        for(int i=0; i<verticalHeight; i++){
-//            neighbour=0;
-//            if(i==0){
-//                if(j==0)
-//                    neighbour=cell[i][j+1]+cell[i+1][j]+cell[i+1][j+1];
-//                else if(j==horizontalWidth-1)
-//                    neighbour=cell[i][j-1]+cell[i+1][j]+cell[i+1][j-1];
-//                else
-//                    neighbour=cell[i][j-1]+cell[i+1][j-1]+cell[i][j+1]+cell[i+1][j]+cell[i+1][j+1];
-//            }
-//            else if(i==verticalHeight-1){
-//                if(j==0)
-//                    neighbour=cell[i][j+1]+cell[i-1][j]+cell[i-1][j+1];
-//                else if(j==horizontalWidth-1)
-//                    neighbour=cell[i][j-1]+cell[i-1][j]+cell[i-1][j-1];
-//                else
-//                    neighbour=cell[i][j-1]+cell[i-1][j-1]+cell[i][j+1]+cell[i-1][j]+cell[i-1][j+1];
-//            }
-//            else if(j==0)
-//                neighbour=cell[i-1][j]+cell[i-1][j+1]+cell[i][j+1]+cell[i+1][j+1]+cell[i+1][j];
-//            else if(j==horizontalWidth-1)
-//                neighbour=cell[i-1][j]+cell[i-1][j-1]+cell[i][j-1]+cell[i+1][j-1]+cell[i+1][j];
-//            else
-//                neighbour=cell[i-1][j]+cell[i-1][j-1]+cell[i][j-1]+cell[i+1][j-1]+cell[i+1][j]+cell[i-1][j+1]+cell[i][j+1]+cell[i+1][j+1];
-//            if(neighbour==3)
-//                buf[i][j]=1;
-//            else if(neighbour==2)
-//                buf[i][j]=cell[i][j];
-//            else
-//                buf[i][j]==0;
-//        }
-//    }
-//    for(int j=0; j<horizontalWidth; j++)
-//        for(int i=0; i<verticalHeight; i++)
-//        	cell[i][j]=buf[i][j];
-//}
-
-int saveState(int cell[][horizontalWidth])
-{
+int saveState(int cell[verticalHeight][horizontalWidth]){
 	int s;
     FILE *st = fopen("state.txt","w+");
     if(st==NULL)
         return -1;
-    for(int y = 0; y < verticalHeight; y++){
-        for(int x = 0; x < horizontalWidth; x++)
+    for(int y=0; y<verticalHeight; y++){
+        for(int x=0; x<horizontalWidth; x++)
 			fprintf(st, "%d", cell[y][x]);
 		fprintf(st,"\n");
 	}
@@ -211,8 +228,8 @@ int saveState(int cell[][horizontalWidth])
 	return 0;
 }
 
-int loadState()
-{
+//have not used this function!
+int loadState(){
 	int s, count=0;
     FILE *st = fopen("state.txt","r+");
     if(st==NULL)
@@ -239,43 +256,85 @@ int loadState()
 	return count;
 }
 
-void evolve(int cell[][horizontalWidth]){
-    int new[verticalHeight][horizontalWidth];
-    int cy, cx;
-    for(cy = 0; cy < verticalHeight; cy++){
-        for(cx = 0; cx < horizontalWidth; cx++){
-            if(cell[cy][cx] == 1 && calculateNeighbour(cell, cx, cy) < 2)
-                new[cy][cx] = 0;
-            else if(cell[cy][cx] == 1 && (calculateNeighbour(cell, cx, cy) == 2 || calculateNeighbour(cell, cx, cy) == 3))
-                new[cy][cx] = 1;
-            else if(cell[cy][cx] == 1 && calculateNeighbour(cell, cx, cy) > 3)
-                new[cy][cx] = 0;
-            else if(cell[cy][cx] == 0 && calculateNeighbour(cell, cx, cy) == 3)
-                new[cy][cx] = 1;
-
+void evolve(int cell[verticalHeight][horizontalWidth]){
+	/*1. Any live cell with 0 or 1 live neighbours becomes dead because of underpopulation
+	2. Any live cell with 2 or 3 live neighbours stays alive because its neighbourhood is just right
+	3. Any live cell with more than 3 live neighbours becomes dead because of overpopulation
+	4. Any dead cell with exactly 3 live neighbours becomes alive by reproduction*/
+	//In short, any cell with 3 around become alive, any cell with 2 stay the same, others become dead
+    int buf[verticalHeight][horizontalWidth];
+    for(int y=0; y<verticalHeight; y++){
+        for(int x=0; x<horizontalWidth; x++){
+            if(calculateNeighbour(cell, x, y)==3)
+                buf[y][x]=1;
+            else if(calculateNeighbour(cell, x, y)==2)
+                buf[y][x]=cell[y][x];
             else
-                new[cy][cx] = 0;
+                buf[y][x]=0;
         }
     }
-    for(cy = 0; cy < verticalHeight; cy++){
-        for(cx = 0; cx < horizontalWidth; cx++){
-            cell[cy][cx] = new[cy][cx];
+    //transfer buf state to cell
+    for(int y=0; y<verticalHeight; y++){
+        for(int x=0; x<horizontalWidth; x++){
+            cell[y][x]=buf[y][x];
         }
     }
 }
-
-int calculateNeighbour(int cell[][horizontalWidth], int x, int y){
-    int count = 0, cx, cy;
-    for(cy = y - 1; cy <= y + 1; cy++){
-        for(cx = x - 1; cx <= x + 1; cx++){
-            if(!(cy < 0 || cx < 0 || cy >= verticalHeight || cx >= horizontalWidth)){
-                if(cell[cy][cx] == 1)
-                    count++;
-                if(cell[cy][cx] == 1 && cx == x && cy == y)
-                    count--;
+ 
+int calculateNeighbour(int cell[verticalHeight][horizontalWidth], int X, int Y){
+    int neighbour=0;
+    for(int y=Y-1; y<=Y+1; y++){
+        for(int x=X-1; x<=X+1; x++){
+            if(y>=0 && x>=0 && y<verticalHeight && x<horizontalWidth){
+            	//count the neighbouring cells
+                if(cell[y][x]==1)
+                    neighbour++;
+                //minus the cell itself
+                if(cell[y][x]==1 && x==X && y==Y)
+                    neighbour--;
             }
         }
     }
-    return count;
+    return neighbour;
 }
 
+//void evolve(int cell[verticalHeight][horizontalWidth]){
+//	int buf[verticalHeight][horizontalWidth];
+//	int neighbour;
+//    for(int y=0; y<verticalHeight; y++){
+//        for(int x=0; x<horizontalWidth; x++){
+//            neighbour=0;
+//            if(y==0){
+//                if(x==0)
+//                    neighbour=cell[y][x+1]+cell[y+1][x]+cell[y+1][x+1];
+//                else if(x==horizontalWidth-1)
+//                    neighbour=cell[y][x-1]+cell[y+1][x]+cell[y+1][x-1];
+//                else
+//                    neighbour=cell[y][x-1]+cell[y+1][x-1]+cell[y][x+1]+cell[y+1][x]+cell[y+1][x+1];
+//            }
+//            else if(y==verticalHeight-1){
+//                if(x==0)
+//                    neighbour=cell[y][x+1]+cell[y-1][x]+cell[y-1][x+1];
+//                else if(x==horizontalWidth-1)
+//                    neighbour=cell[y][x-1]+cell[y-1][x]+cell[y-1][x-1];
+//                else
+//                    neighbour=cell[y][x-1]+cell[y-1][x-1]+cell[y][x+1]+cell[y-1][x]+cell[y-1][x+1];
+//            }
+//            else if(x==0)
+//                neighbour=cell[y-1][x]+cell[y-1][x+1]+cell[y][x+1]+cell[y+1][x+1]+cell[y+1][x];
+//            else if(x==horizontalWidth-1)
+//                neighbour=cell[y-1][x]+cell[y-1][x-1]+cell[y][x-1]+cell[y+1][x-1]+cell[y+1][x];
+//            else
+//                neighbour=cell[y-1][x]+cell[y-1][x-1]+cell[y][x-1]+cell[y+1][x-1]+cell[y+1][x]+cell[y-1][x+1]+cell[y][x+1]+cell[y+1][x+1];
+//            if(neighbour==3)
+//                buf[y][x]=1;
+//            else if(neighbour==2)
+//                buf[y][x]=cell[y][x];
+//            else
+//                buf[y][x]==0;
+//        }
+//    }
+//    for(int y=0; y<verticalHeight; y++)
+//        for(int x=0; x<horizontalWidth; x++)
+//        	cell[y][x]=buf[y][x];
+//}
--- interface.h
+++ interface.h
@@ -5,18 +5,42 @@
 #include <stdbool.h>
 #include <SDL.h>
 
+#define unitSize 10
+
 int horizontalWidth, verticalHeight;
 int screenWidth, screenHeight;
 
+//interface to enter command
 int runInterface();
-void sdlEvent(int cell[][horizontalWidth]); 
+
+//SDL part
+//SDL events containing the following ones
+void sdlEvent(int cell[verticalHeight][horizontalWidth]);
+
+//create SDL window
 SDL_Window *sdlWindow(char *title);
+
+//render SDL window
 SDL_Renderer *rRenderer(SDL_Window *w);
+
+//render checkboard texture on SDL window
 void drawPlate(SDL_Renderer *r);
-void drawCell(SDL_Renderer *r, int cell[][horizontalWidth]);
-void evolve(int cell[][horizontalWidth]);
-int calculateNeighbour(int cell[][horizontalWidth], int x, int y);
-int saveState(int cell[][horizontalWidth]);
+
+//when cell is alive, render the cell
+void drawCell(SDL_Renderer *r, int cell[verticalHeight][horizontalWidth]);
+
+//ALgorithm part
+//use a buf array to evolve the cells
+void evolve(int cell[verticalHeight][horizontalWidth]);
+
+//calculate the state of cell
+int calculateNeighbour(int cell[verticalHeight][horizontalWidth], int x, int y);
+
+//File IO part
+//save currect cell[][] into txt
+int saveState(int cell[verticalHeight][horizontalWidth]);
+
+//load it into algorithms
 int loadState();
 
 #endif
